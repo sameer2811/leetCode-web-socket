b9c15a3f17b54e9ca66580cef1c6ec956558995c
Sameer Singh <sameersingh@playsimple.in>
2024-07-22 02:07:09 +0530

    Adding the change

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://127.0.0.1:5500",
+        origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
         methods: ["GET", "POST"]
     }
 });
